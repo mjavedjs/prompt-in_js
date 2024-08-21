dc0ab99b782172ecb13844b532059b704850eb17
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-08-21 21:43:36 +0500

    javascript_prompt

---
+++ app.js
@@ -0,0 +1,7 @@
+var math=parseInt(prompt("enter your math Marks"));
+var Enghlish=parseInt(prompt("enter your Enghlis Marks"));
+var Chemistry=parseInt(prompt("enter your Chemistery Marks"));
+var Physic=parseInt(prompt("enter your Physic Marks"));
+var obtained=math+Enghlish+Chemistry+Physic;
+var total=400;
+console.log("Your Percentage is", obtained /total*100 +"%");
